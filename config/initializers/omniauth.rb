77bea9fdc814b01974232b51610103e047c85900
mopuriiswaryalakshmi <milakshmi33@gmail.com>
2017-05-24 11:59:25 +0530

    second_commit

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,10 +1,8 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :facebook, ENV['424183687961160'], ENV['212bc9ec72128cd15c00671544575bf8']
+	provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
+  
 end
 
 
-#424183687961160
-#212bc9ec72128cd15c00671544575bf8
-#provider :facebook, ENV['FACEBOOK_APP_ID'], ENV['FACEBOOK_SECRET']
